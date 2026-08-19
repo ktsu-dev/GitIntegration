c0d4811a37e36ff4aef18682b1de348e5c4d672b
Matt Edmondson <matthew.edmondson@gmail.com>
2026-08-19 16:41:34 +1000

    [minor] Swap package manifest to Semantics and add test project

---
+++ GitIntegration.Test/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+// Copyright (c) 2023-2026 ktsu-dev contributors
+
+[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)]
---
+++ GitIntegration.Test/SanityTests.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2023-2026 ktsu-dev contributors
+
+namespace ktsu.GitIntegration.Test;
+
+[TestClass]
+public class SanityTests
+{
+	[TestMethod]
+	public void TestProjectRuns()
+	{
+		Assert.AreEqual(2, 1 + 1);
+	}
+}
